814391a4df869966ff1f025244852fdfccc37be9
Kurler3 <miguelbento2000@gmail.com>
2022-04-04 22:27:59 +0100

    51:42

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useCallback} from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
 
-function App() {
+
+ 
+// REACT FUNCTIONAL COMPONENT
+const App:React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = useCallback((e: React.FormEvent) => {
+      e.preventDefault();
+
+      if(todo) {
+        setTodos([...todos, { id: Date.now(),todo: todo, isDone: false }]);
+        setTodo("");
+      }
+
+  }, [todo, todos]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='heading'>Taskify</span>
+
+      {/* INPUT */}
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={handleAdd}
+      />
+
+      {/* MAPPING OF TODOS */}
+      <TodoList
+          todos={todos}
+          setTodos={setTodos}
+      />
+
     </div>
   );
 }
